c197a1945bc1e07b3b0d68f3723e377ed2171cb0
Onur Ata Asar <onurataasar@gmail.com>
2023-04-16 23:14:31 +0300

    added TabBar

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
 import Home from "./views/app-views/home";
 import Login from "./views/auth-views/login";
-import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import TabBar from "./components/TabBar";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import Notifications from "./views/app-views/notifications";
 
 const Stack = createNativeStackNavigator();
-
+const Tab = createBottomTabNavigator();
 const App = () => {
+  // ...
+
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" options={{ headerShown: false }}>
+      <Tab.Navigator tabBar={(props) => <TabBar {...props} />}>
+        <Tab.Screen
+          name="Login"
+          options={{
+            headerShown: false,
+            tabBarLabel: "Home",
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="home" color={color} size={size} />
+            ),
+          }}
+        >
           {(props) => <Login {...props} />}
-        </Stack.Screen>
-        <Stack.Screen name="Home">
-          {(props) => <Home {...props} />}
-        </Stack.Screen>
-      </Stack.Navigator>
+        </Tab.Screen>
+        <Tab.Screen name="Home">{(props) => <Home {...props} />}</Tab.Screen>
+        <Tab.Screen name="Notifications">
+          {(props) => <Notifications {...props} />}
+        </Tab.Screen>
+      </Tab.Navigator>
     </NavigationContainer>
   );
 };
---
+++ views/app-views/notifications/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View } from "react-native";
+import { Icon } from "react-native-vector-icons/FontAwesome";
+
+const Notifications = () => {
+  return (
+    <View className="flex items-center justify-center">
+      <Text className="text-7xl font-light">Notifications</Text>
+    </View>
+  );
+};
+
+export default Notifications;
